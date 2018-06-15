a786d3afa571db1ccc9029c7417b54f404f33782
Thales @Arquimago <thales@alvesnet.net>
2018-06-15 13:58:17 -0300

    mais ajustes de código

--- app/src/main/java/net/alvesnet/arquimago/listadecompras/AbasAdapter.kt
+++ app/src/main/java/net/alvesnet/arquimago/listadecompras/AbasAdapter.kt
@@ -4,10 +4,9 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 
 class AbasAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
-    var PAGE_COUNT = 8
     var tabTitles = arrayOf("Temperos", "Cozinhar", "Café da Manhã", "Diversos", "Feira", "Limpeza", "Higiene", "Bebidas")
 
-    override fun getCount() = PAGE_COUNT
+    override fun getCount() = tabTitles.size
 
     override fun getItem(position: Int) = ItemFragment.newInstance(position)
 
